38015cfda33a8e88dd8b454b3990cb72f6a5d2db
karuna211204 <karunasreegorrepati@gmail.com>
2025-04-04 14:39:44 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import ContactForm from "./components/Navigation/ContactForm/ContactForm";
+import ContactHeader from "./components/Navigation/ContactHeader/ContactHeader";
+import Navigation from "./components/Navigation/Navigation";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <ContactHeader />
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
